ad11e9ffe13d324e615cc1c085ed22f99703e18c
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-20 19:40:29 +0430

    middleware set up

--- 02-express/app.js
+++ 02-express/app.js
@@ -1,52 +1,14 @@
 const express = require('express');
 const app = express();
-const { products } = require('./data');
+
+// req => middleware => res
 
 app.get('/', (req, res) => {
-    res.send('<h1>Home page</h1><a href="api/products">products</a>')
+    res.send('Home')
 })
 
-app.get('/api/products', (req, res) => {
-    const newProducts = products.map((product) => {
-        const { id, name, image } = product;
-        return { id, name, image }
-    })
-
-    res.json(newProducts)
-})
-
-app.get(`/api/products/:productID`, (req, res) => {
-    const { productID } = req.params
-    const singleProduct = products.find((product) => product.id === Number(productID))
-
-    if (!singleProduct) {
-        return res.status(404).send('Product Does not Exit')
-    }
-    res.json(singleProduct)
-})
-
-app.get('/api/products/:productID/reviews/:reivewID', (req, res) => {
-    res.send('hello world')
-})
-
-app.get('/api/v1/query', (req, res) => {
-    const { search, limit } = req.query
-    let sortedProducts = [...products]
-
-    if (search) {
-        sortedProducts = sortedProducts.filter((product) => {
-            return product.name.startsWith(search)
-        })
-    }
-    if (sortedProducts.length < 1) {
-        // res.status(200).send("no products matched your search")
-        res.status(200).json({ success: true, data: [] })
-    }
-
-    if (limit) {
-        sortedProducts = sortedProducts.slice(0, Number(limit))
-    }
-    res.status(200).json(sortedProducts)
+app.get('/about', (req, res) => {
+    res.send('About')
 })
 
 app.listen(5000, () => {
