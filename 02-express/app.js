2ede947bf3126338876e67e4ec9ceffb2b156da1
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-26 18:35:34 +0430

    adds logger, authorize and morgan

--- 02-express/app.js
+++ 02-express/app.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const app = express();
+const logger = require('./logger');
+const authorize = require('./authorize');
+const morgan = require('morgan')
 
 // req => middleware => res
 
+// app.use([logger, authorize]);
+
+app.use(morgan('tiny'))
+
 app.get('/', (req, res) => {
-    res.send('Home')
+    res.send('Home') 
 })
 
 app.get('/about', (req, res) => {
     res.send('About')
+})
+
+app.get('/api/products', (req, res) => {
+    res.send('Products')
+})
+
+app.get('/api/items', [logger, authorize], (req, res) => {
+    res.send('Items')
 })
 
 app.listen(5000, () => {
