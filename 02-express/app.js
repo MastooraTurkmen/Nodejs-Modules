44d1fe1ac95f597a7ca730471b380d4573276ea4
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-26 19:08:51 +0430

    app js

--- 02-express/app.js
+++ 02-express/app.js
@@ -1,30 +1,7 @@
 const express = require('express');
 const app = express();
-const logger = require('./logger');
-const authorize = require('./authorize');
-const morgan = require('morgan')
 
-// req => middleware => res
-
-// app.use([logger, authorize]);
-
-app.use(morgan('tiny'))
-
-app.get('/', (req, res) => {
-    res.send('Home') 
-})
-
-app.get('/about', (req, res) => {
-    res.send('About')
-})
-
-app.get('/api/products', (req, res) => {
-    res.send('Products')
-})
-
-app.get('/api/items', [logger, authorize], (req, res) => {
-    res.send('Items')
-})
+let { people } = require('./data')
 
 app.listen(5000, () => {
     console.log('Server is listening on port 5000');
