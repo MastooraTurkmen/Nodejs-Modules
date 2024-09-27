1dd3f43abc50f1bc73074893e502718ef941615d
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-27 18:27:05 +0430

    POST Method

--- 02-express/app.js
+++ 02-express/app.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const app = express();
 
-let { people } = require('./data')
+let { people } = require('./data');
+
+// static assets
+app.use(express.static('./methods-public'));
+
+// parse form data
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/api/people', (req, res) => {
+    res.status(200).json({ success: true, data: people })
+})
+
+app.post('/login', (req, res) => {
+    const { name } = req.body;
+    if (name) {
+        return res.status(200).send(`Welcome ${name}`)
+    }
+    res.status(401).send('Please Provide Credentials')
+})
 
 app.listen(5000, () => {
     console.log('Server is listening on port 5000');
