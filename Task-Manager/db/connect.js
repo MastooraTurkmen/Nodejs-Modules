671d49c26ad29c4d4e8ae969a167c148ec68f850
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-29 07:47:02 +0430

    connection string

--- Task-Manager/db/connect.js
+++ Task-Manager/db/connect.js
@@ -1,1 +1,2 @@
-const connectionString
+const connectionString = `
+mongodb+srv://mastooraturkmen:<db_password>@nodeexpressproject.6ma4q.mongodb.net/?retryWrites=true&w=majority&appName=NodeExpressProject`
