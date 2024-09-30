d54c7343fa63dabc61cfa429d355d314802ec024
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-09-30 07:44:00 +0430

    task manager set up

--- Task-Manager/app.js
+++ Task-Manager/app.js
@@ -1,1 +1,26 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+
+// middleware
+
+app.use(express.json())
+
+// routes
+app.get('/hello', (req, res) => {
+    res.send('Task Manager Path');
+})
+
+app.use('/api/v1/tasks', tasks)
+
+// app.get('/api/v1/tasks')        - get all the tasks
+// app.post('/api/v1/tasks')       - create a new task
+// app.get('/api/v1/tasks/:id')    - get single task
+// app.patch('/api/v1/tasks/:id')  - update task
+// app.delete('/api/v1/tasks/:id') - delete task
+
+
+
+const port = 3000
+
+app.listen(port, console.log(`Server is listening on port ${port}`));
