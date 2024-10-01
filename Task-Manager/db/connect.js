a335589c6e4ee32fde12aaa922b14eafe2d5ce9f
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-10-01 20:18:43 +0430

    connectjs

--- Task-Manager/db/connect.js
+++ Task-Manager/db/connect.js
@@ -1,1 +1,12 @@
-const connectionString = 
+const mongoose = require('mongoose')
+
+const connectDB = (url) => {
+    return mongoose.connect(url, {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    }).then(() => console.log('Connected to DB')).catch((err) => console.log(err))
+}
+
+module.exports = connectDB
