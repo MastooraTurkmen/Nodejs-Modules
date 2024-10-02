1372791677c9b23393083f86bc83e2cf14fa2d67
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-10-02 11:04:55 +0430

    adds middleware

--- Task-Manager/app.js
+++ Task-Manager/app.js
@@ -3,6 +3,8 @@ const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config()
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
 // middleware
 app.use(express.json())
@@ -15,15 +17,11 @@ app.get('/hello', (req, res) => {
 
 app.use('/api/v1/tasks', tasks)
 
-// app.get('/api/v1/tasks')        - get all the tasks
-// app.post('/api/v1/tasks')       - create a new task
-// app.get('/api/v1/tasks/:id')    - get single task
-// app.patch('/api/v1/tasks/:id')  - update task
-// app.delete('/api/v1/tasks/:id') - delete task
+app.use(notFound)
+app.use(errorHandlerMiddleware)
 
 
-
-const port = 3000
+const port = process.env.PORT || 3000
 
 const start = async () => {
     try {
