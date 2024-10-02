93f0b96495abe70506b7006da82d09d67a935115
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-10-02 11:05:36 +0430

    async

---
+++ Task-Manager/middleware/async.js
@@ -0,0 +1,12 @@
+const asyncWrappers = (fn) => {
+    return async (req, res, next) => {
+        try {
+            await fn(req, res, next)
+        } catch (error) {
+            next(error)
+        }
+    }
+}
+
+
+module.exports = asyncWrappers;
