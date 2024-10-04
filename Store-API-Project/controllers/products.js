ccb1e94af863a0769aeab3dba04130056fc8398e
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-10-04 20:00:14 +0430

    numeric filters

--- Store-API-Project/controllers/products.js
+++ Store-API-Project/controllers/products.js
@@ -6,7 +6,7 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name, sort, fields } = req.query
+    const { featured, company, name, sort, fields, numericFilters } = req.query
     const queryObject = {}
 
     if (featured) {
@@ -21,6 +21,20 @@ const getAllProducts = async (req, res) => {
         queryObject.name = { $regex: name, $options: 'i' }
     }
 
+    if (numericFilters) {
+        const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<': '$lt',
+            '<=': '$lte',
+        }
+
+        const regEx = /\b(<|>|>=|=|<|<=)\b/g
+        let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`)
+        console.log(filters);
+    }
+
     console.log(queryObject);
 
     let result = Product.find(queryObject)
@@ -33,13 +47,17 @@ const getAllProducts = async (req, res) => {
     }
 
     if (fields) {
-        const fieldsList = fields.split(',').json(' ');
+        const fieldsList = fields.split(',').json(' ')
         result = result.select(fieldsList)
     }
 
-    const products = await result;
- 
+    const page = Number(req.query.page) || 1;
+    const limit = Number(re.query.limit) || 10;
+    const skip = (page - 1) * limit;
 
+    result = result.skip(skip).limit(limit)
+
+    const products = await result;
     res.status(200).json({ products, nbHits: products.length })
 }
 
