9f0dcc4250f769461a6e140fb0ec041ccc876b0b
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-10-05 18:37:38 +0430

    fixed some bugs and adds some feauters

--- Store-API-Project/controllers/products.js
+++ Store-API-Project/controllers/products.js
@@ -32,7 +32,13 @@ const getAllProducts = async (req, res) => {
 
         const regEx = /\b(<|>|>=|=|<|<=)\b/g
         let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`)
-        console.log(filters);
+        const options = ['price', 'rating'];
+        filters = filters.split(',').forEach((item) => {
+            const [field, operator, value] = item.split('-')
+            if (options.includes(field)) {
+                queryObject[field] = { [operator]: Number(value) }
+            }
+        })
     }
 
     console.log(queryObject);
@@ -40,19 +46,19 @@ const getAllProducts = async (req, res) => {
     let result = Product.find(queryObject)
     // sort
     if (sort) {
-        const sortList = sort.split(',').json(' ');
+        const sortList = sort.split(',').join(' ');
         result = result.sort(sortList)
     } else {
         result = result.sort('createAt')
     }
 
     if (fields) {
-        const fieldsList = fields.split(',').json(' ')
+        const fieldsList = fields.split(',').join(' ')
         result = result.select(fieldsList)
     }
 
     const page = Number(req.query.page) || 1;
-    const limit = Number(re.query.limit) || 10;
+    const limit = Number(req.query.limit) || 10;
     const skip = (page - 1) * limit;
 
     result = result.skip(skip).limit(limit)
