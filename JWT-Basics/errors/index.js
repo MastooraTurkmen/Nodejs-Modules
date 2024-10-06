e20d52e1844152f3f729a424700eaa9e6eb11c2e
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-10-06 15:43:34 +0430

    json web token: bad request

---
+++ JWT-Basics/errors/index.js
@@ -0,0 +1,6 @@
+const CustomAPIError = require('./custom-error')
+const BadRequestError = require('./bad-request')
+const UnauthenticatedError = require('./unauthenticated')
+
+
+module.exports = { CustomAPIError, BadRequestError, UnauthenticatedError }
