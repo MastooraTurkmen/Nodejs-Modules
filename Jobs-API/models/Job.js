eb2c17c9e9be947568abfe81f208a1dc0076683e
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-10-08 07:25:24 +0430

    create job

---
+++ Jobs-API/models/Job.js
@@ -0,0 +1,27 @@
+const { required } = require('joi')
+const mongoose = require('mongoose')
+
+const JobSchema = new mongoose.Schema({
+    company: {
+        type: String,
+        required: [true, 'Please provide the company name'],
+        maxlength: 50
+    },
+    position: {
+        type: String,
+        required: [true, 'Please provide the position'],
+        maxlength: 100
+    },
+    status: {
+        type: String,
+        enum: ['interview', 'declined', 'pending'],
+        default: 'pending'
+    },
+    createdBy: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'Please provide user']
+    }
+}, { timestamps: true })
+
+module.exports = mongoose.model('Job', JobSchema)
