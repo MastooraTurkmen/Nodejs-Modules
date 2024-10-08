481fc46571cac0ce0e4c8e256e3f142e4d1c9873
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-10-08 07:25:18 +0430

    create job

--- Jobs-API/app.js
+++ Jobs-API/app.js
@@ -5,6 +5,7 @@ const app = express();
 
 // connectDB
 const connectDB = require('./db/connect')
+const authorizateUser = require('./middleware/authentication')
 
 // routers
 const authRouter = require('./routes/auth')
@@ -19,7 +20,7 @@ app.use(express.json());
 
 // routes
 app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/jobs', jobsRouter)
+app.use('/api/v1/jobs', authorizateUser, jobsRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
