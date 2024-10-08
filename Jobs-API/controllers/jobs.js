66818b61f9978aefc02979fe0eadfa03451b8014
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-10-08 07:25:21 +0430

    create job

--- Jobs-API/controllers/jobs.js
+++ Jobs-API/controllers/jobs.js
@@ -1,3 +1,8 @@
+const Job = require('../models/Job')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError, NotFoundError } = require('../errors')
+
+
 const getAllJobs = async (req, res) => {
     res.send('getAllJobs user')
 }
@@ -7,7 +12,9 @@ const getJob = async (req, res) => {
 }
 
 const createJob = async (req, res) => {
-    res.send('create job')
+    req.body.createdBy = req.user.userId
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json(job)
 }
 
 const updateJob = async (req, res) => {
