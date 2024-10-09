21a6189caf6976f5eb5dd74d9fd1afae37e6ba6d
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-10-09 11:06:08 +0430

    handle error

--- Jobs-API/app.js
+++ Jobs-API/app.js
@@ -3,6 +3,13 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+// extra security packages
+
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 // connectDB
 const connectDB = require('./db/connect')
 const authorizateUser = require('./middleware/authentication')
@@ -15,7 +22,15 @@ const jobsRouter = require('./routes/jobs')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1)
+app.use(rateLimiter({
+  windowsMs: 15 * 60 * 1000,
+  max: 100
+}))
 app.use(express.json());
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 // extra packages
 
 // routes
