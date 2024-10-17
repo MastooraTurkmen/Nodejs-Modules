51372c2f6316acf13059ec261703d8d586d0ede4
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-10-17 22:06:02 +0430

    stipe payment

---
+++ Stripe-Payment/controllers/stripeController.js
@@ -0,0 +1,20 @@
+const stripe = require('stripe')(process.env.STRIPE_KEY)
+
+const stripeController = async (req, res) => {
+    const { purchase, total_amount, shipping_fee } = req.body;
+
+    const calculateOrderAmount = () => {
+        return total_amount + shipping_fee
+    }
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: calculateOrderAmount(),
+        currency: 'usd'
+    })
+
+    console.log(paymentIntent);
+
+    res.json({ clientSecret: paymentIntent.client_secret })
+}
+
+module.exports = stripeController;
