dfac6ac6ec1ecf7338579bb4232bd4d466240959
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-10-19 17:18:31 +0430

    set up

--- E-Commerce-API/app.js
+++ E-Commerce-API/app.js
@@ -1,1 +1,15 @@
-console.log('E-Commerce API');
+const express = require('express')
+const app = express()
+
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        app.listen(port, console.log(`Server is listening on Port ${port}`))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
