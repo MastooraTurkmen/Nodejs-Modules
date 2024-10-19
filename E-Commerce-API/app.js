cb8b57d3a649e23f0d942c3ee9713bef117fa567
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-10-19 18:46:49 +0430

    middleware

--- E-Commerce-API/app.js
+++ E-Commerce-API/app.js
@@ -1,11 +1,32 @@
+require('dotenv').config()
+require('express-async-errors')
+
 const express = require('express')
 const app = express()
 
+// database
+const connectDB = require('./db/connect')
+
+// middleware
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+app.use(express.json())
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.send('e commerce api')
+})
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 3000;
 
 const start = async () => {
     try {
+        await connectDB(process.env.MONGO_URL)
         app.listen(port, console.log(`Server is listening on Port ${port}`))
     } catch (error) {
         console.log(error);
