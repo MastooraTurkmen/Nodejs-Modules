b132eef3c14dc6afb6ef77fe0eed14b759d5cb0c
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-10-20 21:05:12 +0430

    Auth Routes Structure

--- E-Commerce-API/app.js
+++ E-Commerce-API/app.js
@@ -3,6 +3,8 @@ require('express-async-errors')
 
 const express = require('express')
 const app = express()
+const morgon = require('morgan')
+const authRoutes = require('./routes/authRoutes')
 
 // database
 const connectDB = require('./db/connect')
@@ -12,8 +14,8 @@ const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
 app.use(express.json())
-app.use(express.json())
-
+app.use(morgon('tiny'))
+app.use('/api/v1/auth', authRoutes)
 
 app.get('/', (req, res) => {
     res.send('e commerce api')
@@ -26,7 +28,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
     try {
-        await connectDB(process.env.MONGO_URL)
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, console.log(`Server is listening on Port ${port}`))
     } catch (error) {
         console.log(error);
