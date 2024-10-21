7e26cd8ae4c5bc13ad3a8528e3dfc4af87fd46bd
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-10-21 14:00:09 +0430

    register user

--- E-Commerce-API/controllers/authController.js
+++ E-Commerce-API/controllers/authController.js
@@ -1,5 +1,20 @@
+const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes')
+const CustomError = require('../errors')
+
 const register = async (req, res) => {
-    res.send('User registered')
+    const { email, name, password } = req.body;
+    const emailAlreadyExists = await User.findOne({ email })
+
+    if (emailAlreadyExists) {
+        throw new CustomError.BadRequestError('Email already exists')
+    }
+
+    const isFirstAccount = await User.countDocuments({}) === 0;
+    const role = isFirstAccount ? 'admin' : "user"
+
+    const user = await User.create({ name, email, password, role })
+    res.status(StatusCodes.CREATED).json({ user })
 }
 
 const login = async (req, res) => {
