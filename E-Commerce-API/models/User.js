8335c93330c6cf7277d3f6ff8a5d4c8824832f40
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-10-22 06:15:34 +0430

    bcrypt

--- E-Commerce-API/models/User.js
+++ E-Commerce-API/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -27,7 +28,17 @@ const UserSchema = new mongoose.Schema({
         enum: ['admin', 'user'],
         default: 'user'
     }
-})
+});
+
+UserSchema.pre('save', async function () {
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+});
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+    const isMatch = await bcrypt.compare(candidatePassword, this.password)
+    return isMatch;
+}
 
 
 module.exports = mongoose.model('User', UserSchema)
