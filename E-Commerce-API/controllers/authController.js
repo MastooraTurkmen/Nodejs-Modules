3a0833af56a5663221687adb84c5a45c25d48a2f
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-10-25 16:29:42 +0430

    auth controller

--- E-Commerce-API/controllers/authController.js
+++ E-Commerce-API/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
+const { createJWT } = require('../utils')
 const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
@@ -16,7 +17,7 @@ const register = async (req, res) => {
 
     const user = await User.create({ name, email, password, role })
     const tokenUser = { name: user.name, userId: user._id, role: user.role }
-    const token = jwt.sign(tokenUser, 'jwtSecret', { expiresIn: '1d' })
+    const token = createJWT({ payload: tokenUser })
     res.status(StatusCodes.CREATED).json({ user: tokenUser })
 }
 
