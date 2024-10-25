d15ff3aaf46c68ae6f2c1e038c90fd6b7515fe6a
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-10-25 16:29:52 +0430

    jwt js

---
+++ E-Commerce-API/utils/jwt.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken')
+
+const createJWT = ({ payload }) => {
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_LIFETIME
+    })
+
+    return token
+}
+
+const isTokenValid = ({ token }) => jwt.verify(token, process.env.JWT_SECRET)
+
+module.exports = {
+    createJWT,
+    isTokenValid
+}
