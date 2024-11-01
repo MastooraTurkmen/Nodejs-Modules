018e435f388b25b9150f91a24f25013ba69c5186
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-01 20:27:29 +0430

    app js

--- Cookies-Example/app.js
+++ Cookies-Example/app.js
@@ -1,1 +1,48 @@
-console.log('E-Commerce API');
+require('dotenv').config()
+require('express-async-errors')
+
+const express = require('express')
+const app = express()
+const morgon = require('morgan')
+const authRoutes = require('./routes/authRoutes')
+
+// database
+const connectDB = require('./db/connect')
+
+// middleware
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+const cookieParser = require('cookie-parser')
+
+app.use(morgon('tiny'))
+app.use(express.json())
+app.use(cookieParser(process.env.JWT_SECRET))
+app.use(express.static('./public'))
+
+app.get('/', (req, res) => {
+    res.send('e commerce api')
+})
+
+app.get('/api/v1', (req, res) => {
+    res.send('e commerce api')
+})
+
+app.get('/api/v1/auth', (req, res) => {
+    res.send('e commerce api')
+})
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening on Port ${port}`))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
