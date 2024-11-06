edfec5322187ca0773b615e929a8fa5146b2e5bb
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-06 21:13:45 +0430

    cookies set up

--- E-Commerce-API/controllers/authController.js
+++ E-Commerce-API/controllers/authController.js
@@ -18,7 +18,15 @@ const register = async (req, res) => {
     const user = await User.create({ name, email, password, role })
     const tokenUser = { name: user.name, userId: user._id, role: user.role }
     const token = createJWT({ payload: tokenUser })
+
+
+    const oneDay = 1000 * 60 * 60 * 24
+    res.cookie('token', token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + oneDay)
+    })
     res.status(StatusCodes.CREATED).json({ user: tokenUser })
+
 }
 
 const login = async (req, res) => {
