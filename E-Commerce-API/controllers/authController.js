1aba909c8f3f5ff583e2d1b5b40d360a7c2371c5
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-07 20:54:22 +0430

    set up cookies

--- E-Commerce-API/controllers/authController.js
+++ E-Commerce-API/controllers/authController.js
@@ -1,7 +1,7 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
-const { createJWT } = require('../utils')
+const { attachCookiesToResponse } = require('../utils')
 const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
@@ -17,16 +17,8 @@ const register = async (req, res) => {
 
     const user = await User.create({ name, email, password, role })
     const tokenUser = { name: user.name, userId: user._id, role: user.role }
-    const token = createJWT({ payload: tokenUser })
-
-
-    const oneDay = 1000 * 60 * 60 * 24
-    res.cookie('token', token, {
-        httpOnly: true,
-        expires: new Date(Date.now() + oneDay)
-    })
+    attachCookiesToResponse({ res, user: tokenUser })
     res.status(StatusCodes.CREATED).json({ user: tokenUser })
-
 }
 
 const login = async (req, res) => {
