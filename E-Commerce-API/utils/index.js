d2e20379fd75b7a5ff2de8892d15bd677b3ceac6
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-07 20:54:26 +0430

    set up cookies

--- E-Commerce-API/utils/index.js
+++ E-Commerce-API/utils/index.js
@@ -1,3 +1,3 @@
-const { createJWT, isTokenValid } = require('./jwt')
+const { createJWT, isTokenValid, attachCookiesToResponse } = require('./jwt')
 
-module.exports = { createJWT, isTokenValid }  
+module.exports = { createJWT, isTokenValid, attachCookiesToResponse }  
