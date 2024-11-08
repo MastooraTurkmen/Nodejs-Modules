28cb4d082d226fea87c02dfaef13c203816d4132
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-08 19:37:47 +0430

    logout set up

--- E-Commerce-API/utils/jwt.js
+++ E-Commerce-API/utils/jwt.js
@@ -17,7 +17,9 @@ const attachCookiesToResponse = ({ res, user }) => {
     const oneDay = 1000 * 60 * 60 * 24
     res.cookie('token', token, {
         httpOnly: true,
-        expires: new Date(Date.now() + oneDay)
+        expires: new Date(Date.now() + oneDay),
+        secure: process.env.NODE_ENV === 'production',
+        signed: true,
     })
 }
 
