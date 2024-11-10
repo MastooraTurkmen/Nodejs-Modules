3b602aae81639281c974d2d45f6c810841b1e2da
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-10 21:11:07 +0430

    authorizePermission

--- E-Commerce-API/routes/userRoute.js
+++ E-Commerce-API/routes/userRoute.js
@@ -1,14 +1,14 @@
 const { getAllUsers, getSingleUser, updateUser, updateUserPassword, showCurrentUser } = require('../controllers/userController')
 const express = require('express')
 const router = express.Router()
-const { authenticateUser } = require('../middleware/authentication')
+const { authenticateUser, authorizePermission } = require('../middleware/authentication')
 
-router.route('/').get(getAllUsers);
+router.route('/').get(authenticateUser, authorizePermission, getAllUsers);
 
 router.route('/showMe').get(showCurrentUser);
 router.route('/updateUserPassword').patch(updateUserPassword)
 router.route('/updateUser').patch(updateUser)
 
-router.route('/:id').get(getSingleUser);
+router.route('/:id').get(authenticateUser, getSingleUser);
 
 module.exports = router
