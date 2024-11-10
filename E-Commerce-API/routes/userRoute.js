37adba572b16e2d44a6c5a5398313b0941e712bf
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-10 15:51:34 +0430

    rename to patch

--- E-Commerce-API/routes/userRoute.js
+++ E-Commerce-API/routes/userRoute.js
@@ -3,9 +3,11 @@ const express = require('express')
 const router = express.Router()
 
 router.route('/').get(getAllUsers);
+
 router.route('/showMe').get(showCurrentUser);
+router.route('/updateUserPassword').patch(updateUserPassword)
+router.route('/updateUser').patch(updateUser)
+
 router.route('/:id').get(getSingleUser);
-router.route('/updateUserPassword').post(updateUserPassword)
-router.route('/updateUser').post(updateUser)
 
 module.exports = router
