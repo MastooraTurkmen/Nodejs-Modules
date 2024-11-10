a0ee14e1d18899585a1e8ed772ca0dc50287094a
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-10 20:01:04 +0430

    authentication

--- E-Commerce-API/middleware/authentication.js
+++ E-Commerce-API/middleware/authentication.js
@@ -2,5 +2,19 @@ const CustomErr = require('../errors')
 const { isTokenValid } = require('../utils')
 
 const authenticateUser = async (req, res, next) => {
-    
+    const token = req.signedCookies.toekn
+    if (!token) {
+        throw new CustomErr.UnauthenticatedError("Authentication Invalid")
+    }
+
+    try {
+        const { name, userId, role } = isTokenValid({ token })
+        req.user = { name, userId, role }
+        console.log(payload);
+        next()
+    } catch (error) {
+        throw new CustomErr.UnauthenticatedError("Authentication Invalid")
+    }
 }
+
+module.exports = { authenticateUser }
