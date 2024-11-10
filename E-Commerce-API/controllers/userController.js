d7f7303cd49b450d81c1307e4feaab4bba31b436
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-10 20:00:43 +0430

    user controller

--- E-Commerce-API/controllers/userController.js
+++ E-Commerce-API/controllers/userController.js
@@ -1,20 +1,29 @@
+const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes')
+const CustomError = require('../errors')
+
 const getAllUsers = async (req, res) => {
-    res.send('get all users route')
+    const users = await User.find({ role: 'user' }).select('-password')
+    res.status(StatusCodes.OK).json({ users })
 }
 
 const getSingleUser = async (req, res) => {
-    res.send('get sing users route')
+    const user = await User.findOne({ _id: req.params.id }).select('-password')
+
+    if (!user) {
+        throw new CustomError.NotFoundError(`No user with id: ${req.params.id}`)
+    }
 }
 const showCurrentUser = async (req, res) => {
     res.send('show current users route')
 }
 
 const updateUser = async (req, res) => {
-    res.send('update users route')
+    res.send(req.body)
 }
 
 const updateUserPassword = async (req, res) => {
-    res.send('get all users route')
+    res.send(req.body)
 }
 
 module.exports = {
