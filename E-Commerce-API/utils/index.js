a253051bd909bfd9acebb669b30b654a65032398
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-12 20:52:36 +0430

    import exports

--- E-Commerce-API/utils/index.js
+++ E-Commerce-API/utils/index.js
@@ -1,3 +1,5 @@
 const { createJWT, isTokenValid, attachCookiesToResponse } = require('./jwt')
+const createTokenUser = require('./createTokenUser')
+const checkPermissions = require('./checkPermissions')
 
-module.exports = { createJWT, isTokenValid, attachCookiesToResponse }  
+module.exports = { createJWT, isTokenValid, attachCookiesToResponse, createTokenUser, checkPermissions }  
