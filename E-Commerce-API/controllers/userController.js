f0ab422c3fe3f9646b603009f7ca71b0f040d358
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-12 20:51:47 +0430

    user controller

--- E-Commerce-API/controllers/userController.js
+++ E-Commerce-API/controllers/userController.js
@@ -1,6 +1,8 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
+const { attachCookiesToResponse, createTokenUser, checkPermissions } = require('../utils')
+
 
 const getAllUsers = async (req, res) => {
     const users = await User.find({ role: 'user' }).select('-password')
@@ -13,9 +15,13 @@ const getSingleUser = async (req, res) => {
     if (!user) {
         throw new CustomError.NotFoundError(`No user with id: ${req.params.id}`)
     }
+
+    checkPermissions(req.user, user._id)
+    res.status(StatusCodes.OK).json({ user })
 }
+
 const showCurrentUser = async (req, res) => {
-    res.send('show current users route')
+    res.status(StatusCodes.OK).json({ user: req.user })
 }
 
 const updateUser = async (req, res) => {
@@ -23,7 +29,22 @@ const updateUser = async (req, res) => {
 }
 
 const updateUserPassword = async (req, res) => {
-    res.send(req.body)
+    const { oldPassword, newPassword } = req.body;
+    if (!oldPassword || !newPassword) {
+        throw new CustomError.BadRequestError('Please provide both values')
+    }
+
+    const user = await User.findOne({ _id: req.user.userId })
+
+    const isPasswordCorrect = await user.comparePassword(oldPassword)
+
+    if (!isPasswordCorrect) {
+        throw new CustomError.UnauthenticatedError('Invalid Credentials')
+    }
+
+    user.password = newPassword;
+    await user.save()
+    res.status(StatusCodes.OK).json({ msg: "Success! Password Updated." })
 }
 
 module.exports = {
