755dcc0cee2065f23c7ad85102ada89548326ccd
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-16 20:37:02 +0430

    product router

--- E-Commerce-API/routes/productRoutes.js
+++ E-Commerce-API/routes/productRoutes.js
@@ -3,3 +3,14 @@ const router = express.Router()
 const { authenticateUser, authorizePermission } = require('../middleware/authentication')
 
 const { deleteProduct, updateProduct, uploadImage, getAllProducts, getSingleProduct, createProduct } = require('../controllers/productController')
+
+router.route('/').post([authenticateUser, authorizePermission('admin')], createProduct).get(getAllProducts)
+
+router.route('/uploadImage').post([authenticateUser, authorizePermission('admin')], uploadImage)
+
+router.route('/:id')
+    .get(getSingleProduct)
+    .patch([authenticateUser, authorizePermission('admin')], updateProduct)
+    .delete([authenticateUser, authorizePermission('admin')], deleteProduct)
+
+module.exports = router
