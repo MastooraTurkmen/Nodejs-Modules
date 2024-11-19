b63fad41f1c23ba88801238254e6600ffc127350
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-19 16:44:30 +0430

    delete review

--- E-Commerce-API/controllers/reviewController.js
+++ E-Commerce-API/controllers/reviewController.js
@@ -47,7 +47,16 @@ const updateReview = async (req, res) => {
 }
 
 const deleteReview = async (req, res) => {
-    res.send('delete review')
+    const { id: reviewId } = req.params;
+    const review = await Review.findOne({ _id: reviewId })
+
+    if (!review) {
+        throw new CustomError.NotFoundError(`No review with id ${reviewId}`)
+    }
+
+    checkPermissions(req.user, review.user)
+    await review.remove()
+    res.status(StatusCodes.OK).json({ msg: 'Success! Review Removed' })
 }
 
 module.exports = { createReview, updateReview, getAllReviews, getSingleReview, deleteReview }
