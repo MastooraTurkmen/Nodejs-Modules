6916ff1b90e0a46ec22a45a933d3b22605733b82
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-19 16:22:23 +0430

    get review

--- E-Commerce-API/controllers/reviewController.js
+++ E-Commerce-API/controllers/reviewController.js
@@ -28,11 +28,18 @@ const createReview = async (req, res) => {
 }
 
 const getAllReviews = async (req, res) => {
-    res.send('get all reviews')
+    const reviews = await Review.find({})
+    res.status(StatusCodes.OK).json({ reviews, count: reviews.length })
 }
 
 const getSingleReview = async (req, res) => {
-    res.send('get single review')
+    const { id: reviewId } = req.params;
+    const review = await Review.findOne({ _id: reviewId })
+
+    if (!review) {
+        throw new CustomError.NotFoundError(`No review with id ${reviewId}`)
+    }
+    res.status(StatusCodes.OK).json({ review })
 }
 
 const updateReview = async (req, res) => {
