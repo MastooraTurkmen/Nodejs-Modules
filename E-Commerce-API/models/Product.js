e6e44c08307704941fe07b5b3f17e46499e95e50
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-20 18:24:31 +0430

    remove reviews

--- E-Commerce-API/models/Product.js
+++ E-Commerce-API/models/Product.js
@@ -55,13 +55,28 @@ const ProductSchema = new mongoose.Schema({
         type: Number,
         default: 0,
     },
+    numOfReviews: {
+        type: Number,
+        default: 0,
+    },
     user: {
         type: mongoose.Types.ObjectId,
         ref: 'User',
         required: true,
     }
 }, {
-    timestamps: true
+    timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true }
+})
+
+ProductSchema.virtual('reviews', {
+    ref: "Review",
+    localField: "_id",
+    foreignField: "product",
+    justOne: false
+})
+
+ProductSchema.pre('remove', async function (next) {
+    await this.model('Review').deleteMany({ product: this._id })
 })
 
 module.exports = mongoose.model('Product', ProductSchema)
