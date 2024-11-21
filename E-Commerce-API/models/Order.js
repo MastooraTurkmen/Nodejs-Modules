7c9ff9f51375c3e4173dba37c567b3f63d164e4d
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-21 20:13:02 +0430

    Order Schema

---
+++ E-Commerce-API/models/Order.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose')
+
+const SingleCartItemSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    product: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+        required: true
+    }
+})
+
+const OrderSchema = mongoose.Schema({
+    tax: {
+        type: Number,
+        required: true
+    },
+    shippingFee: {
+        type: Number,
+        required: true
+    },
+    subtotal: {
+        type: Number,
+        required: true
+    },
+    total: {
+        type: Number,
+        required: true
+    },
+    cartItems: [SingleCartItemSchema],
+    status: {
+        type: String,
+        enum: ['pending', 'failed', 'delivered', 'canceled'],
+        default: 'pending'
+    }
+    ,
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    clientSecret: {
+        type: String,
+        require: true
+    },
+    paymentIntendId: {
+        type: String,
+    },
+}, { timestamps: true })
+
+module.exports = mongoose.model('Order', OrderSchema)
