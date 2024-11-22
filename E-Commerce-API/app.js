71b99387ae5bf4ed86dcb283d3b73027a13361b6
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-22 15:05:19 +0430

    install security

--- E-Commerce-API/app.js
+++ E-Commerce-API/app.js
@@ -7,6 +7,11 @@ const app = express()
 const morgon = require('morgan')
 const cookiesParser = require('cookie-parser')
 const fileUpload = require('express-fileupload')
+const rateLimiter = require('express-rate-limit')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const cors = require('cors')
+const mongoSanitize = require('express-mongo-sanitize')
 
 // router
 const authRoutes = require('./routes/authRoutes')
@@ -21,6 +26,16 @@ const connectDB = require('./db/connect')
 // middleware
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
+
+app.set('trust proxy', 1)
+app.use(rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60
+}))
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+app.use(mongoSanitize())
 
 app.use(express.json())
 app.use(morgon('tiny'))
