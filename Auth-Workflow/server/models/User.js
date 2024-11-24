697678f01430a3189578697f28ddf226f16fea4a
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-24 21:20:10 +0430

    user

--- Auth-Workflow/server/models/User.js
+++ Auth-Workflow/server/models/User.js
@@ -33,7 +33,13 @@ const UserSchema = new mongoose.Schema({
     type: Boolean,
     default: false
   },
-  verified: Date
+  verified: Date,
+  passwordToken: {
+    type: String,
+  },
+  passwordTokenExpirationDate: {
+    type: Date,
+  }
 });
 
 UserSchema.pre('save', async function () {
