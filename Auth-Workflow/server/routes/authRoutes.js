502bd16ad665a330a2dbe21b4ad296b62847b628
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-24 21:20:16 +0430

    auth routes

--- Auth-Workflow/server/routes/authRoutes.js
+++ Auth-Workflow/server/routes/authRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const { register, login, logout, verifyEmail } = require('../controllers/authController');
+const { authenticateUser } = require('../middleware/authentication');
+
+const { register, login, logout, verifyEmail, forgotPassword, resetPassword } = require('../controllers/authController');
 
 router.post('/register', register);
 router.post('/login', login);
-router.get('/logout', logout);
+router.get('/logout', authenticateUser, logout);
 router.post('/verify-email', verifyEmail)
+router.post('/reset-password', resetPassword)
+router.post('/forgot-password', forgotPassword)
 
 module.exports = router;
