f05738042565874b7b87094ae5682cee6b1184b7
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-25 06:40:50 +0430

    hash password, reset & forgot password,

--- Auth-Workflow/server/utils/index.js
+++ Auth-Workflow/server/utils/index.js
@@ -2,11 +2,16 @@ const { createJWT, isTokenValid, attachCookiesToResponse } = require('./jwt');
 const createTokenUser = require('./createTokenUser');
 const checkPermissions = require('./checkPermissions');
 const sendVerificationEmail = require('./sendVerificationEmail')
+const sendResetPasswordEmail = require('./sendResetPasswordEmail')
+const createHash = require('../utils/createHash')
+
 module.exports = {
   createJWT,
   isTokenValid,
   attachCookiesToResponse,
   createTokenUser,
   checkPermissions,
-  sendVerificationEmail
+  sendVerificationEmail,
+  sendResetPasswordEmail,
+  createHash
 };
