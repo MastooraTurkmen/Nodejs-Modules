ae039e9dc8850fe8eeb0b13ac7e4845cd9bfc883
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-11-25 06:40:40 +0430

    hash password, reset & forgot password,

--- Auth-Workflow/server/controllers/authController.js
+++ Auth-Workflow/server/controllers/authController.js
@@ -2,7 +2,7 @@ const User = require('../models/User');
 const Token = require('../models/Token');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
-const { attachCookiesToResponse, createTokenUser, sendVerificationEmail } = require('../utils');
+const { attachCookiesToResponse, createTokenUser, sendVerificationEmail, sendResetPasswordEmail, createHash } = require('../utils');
 const crypto = require('crypto')
 const sendEmail = require('../utils/sendEmail')
 
@@ -23,7 +23,6 @@ const register = async (req, res) => {
   const user = await User.create({ name, email, password, role, verificationToken });
 
   const origin = 'http://localhost:3000';
-  const newOrigin = 'https://react-node-user-workflow-front-end.netlify.app/'
 
   await sendVerificationEmail({
     name: user.name,
@@ -130,10 +129,15 @@ const forgotPassword = async (req, res) => {
   const user = await User.findOne({ email })
   if (user) {
     const passowrdToken = crypto.randomBytes(70).toString('hex')
+
+    // send email
+    const origin = 'http://localhost:3000';
+    await sendResetPasswordEmail({ name: user.name, email: user.email, token: passowrdToken })
+
     const tenMinutes = 1000 * 60 * 10
     const passwordTokenExpirationDate = new Date(Date.now() + tenMinutes)
 
-    user.passwordToken = passowrdToken
+    user.passwordToken = createHash(passowrdToken)
     user.passwordTokenExpirationDate = passwordTokenExpirationDate
     await user.save()
   }
@@ -141,7 +145,22 @@ const forgotPassword = async (req, res) => {
 }
 
 const resetPassword = async (req, res) => {
-  res.send('Forgot passowrd')
+  const { token, email, password } = req.body
+  if (!token || !email || !password) {
+    throw new CustomError.BadRequestError('Please provide all values');
+  }
+  const user = await User.findOne({ email })
+
+  if (user) {
+    const currentDate = new Date()
+    if (user.passwordToken === createHash(token) && user.passwordTokenExpirationDate > currentDate) {
+      user.password = password
+      user.passwordToken = null
+      user.passwordTokenExpirationDate = null
+      await user.save()
+    }
+  }
+  res.status(StatusCodes.OK).json({})
 }
 
 module.exports = {
